0097b92cc367d6ca5129872b64953a5280444dbc
NolifekNTB <kacperkotlowskikk1@gmail.com>
2024-04-25 09:41:04 +0200

    add folder structure

--- app/src/main/java/com/example/bankapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/bankapp/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.bankapp
+package com.example.bankapp.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.bankapp.ui.theme.BankAppTheme
+import com.example.bankapp.presentation.ui.theme.BankAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/bankapp/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/bankapp/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.bankapp.ui.theme
+package com.example.bankapp.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
